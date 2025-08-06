109d8c871345691357da698b9edcd6196b9a560f
Sroopesh1122 <shivuroopesh6362@gmail.com>
2025-08-06 11:29:37 +0530

    svg changes

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <h1>Shivu Rooes hEre</h1>
       <h1>Vite + React {apiurl}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
